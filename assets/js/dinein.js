2cf74c013906470f4cc58478d6b32d84eb2d2c15
Jenner Garcia <jenner.garcia@gmail.com>
2021-01-02 22:26:11 -0500

    yelp API start

--- assets/js/dinein.js
+++ assets/js/dinein.js
@@ -1,4 +1,3 @@
-
 var apiKey = "214f60134dc9416a9544280f08aa9f0b"
 
 //filter variables
@@ -11,26 +10,26 @@ var recipes = [];
 $(".result").hide(); //Hide results divs on page load
 
 //getFilters() function is called when the searchBtn is clicked. The filter values are taken from the left-hand filters and stored in an object "parameters". It is then passed to the getQueryURL() function
-function getFilters(){
+function getFilters() {
 
     //get selected ingredients from #ingredientSelections
     var ingredients = [];
-    $("#ingredientSelections input:checked").each(function(){
+    $("#ingredientSelections input:checked").each(function() {
         ingredients.push($(this).attr("id"));
     })
-        console.log(ingredients); //testing
+    console.log(ingredients); //testing
 
     //get recipe name from #nameInput
     var name = $("#nameInput").val();
-        console.log(name); //testing
+    console.log(name); //testing
 
     //get selected cuisine from #cusineSelections
     var cuisine = $("#cuisineSelections").val();
-        console.log(cuisine); //testing
+    console.log(cuisine); //testing
 
     //get selected category from #categorySelections
     var category = $("#categorySelections").val();
-        console.log(category); //testing
+    console.log(category); //testing
 
     //create parameters object to hold all parameter values stored from filters
     var parameters = {
@@ -42,31 +41,31 @@ function getFilters(){
 
     createQueryURL(parameters); //call createQueryURL and pass in parameters object
 
-}//End of getFilters()
+} //End of getFilters()
 
 //createQueryURL() takes in an object and creates a queryURL based on the parameters passed in.  It then passes the URL to the getResults() function
-function createQueryURL(params){
+function createQueryURL(params) {
 
-    var queryURL1 ="https://api.spoonacular.com/recipes/complexSearch?apiKey=" + apiKey + "&includeNutrition=true&addRecipeInformation=true&includeIngredients=" + params.ingredients.toString() + "&titleMatch="+ params.name + "&cuisine=" + params.cuisine + "&type=" + params.category;
+    var queryURL1 = "https://api.spoonacular.com/recipes/complexSearch?apiKey=" + apiKey + "&includeNutrition=true&addRecipeInformation=true&includeIngredients=" + params.ingredients.toString() + "&titleMatch=" + params.name + "&cuisine=" + params.cuisine + "&type=" + params.category;
 
     console.log(queryURL1); //testing
 
     getResults(queryURL1); //Pass query URL to the getResults function
 
-}//End of createQueryURL()
+} //End of createQueryURL()
 
 //getResults() function takes in the queryURL and makes the AJAX call.  The response is then passed to the populateRecipes function
-function getResults(url){
+function getResults(url) {
 
     $.ajax({
-    url: url,
-    method: "GET"
+        url: url,
+        method: "GET"
     }).then(function(response) {
         console.log(response);
 
     });
 
-}//End of getResults()
+} //End of getResults()
 
 // function getRecipeDetails(ids){
 
@@ -87,21 +86,21 @@ function getResults(url){
 //     }
 // }
 
-function populateResults(recipes){
+function populateResults(recipes) {
     console.log(recipes);
     var j = 1;
 
-    for (i = 0; i < limit; i++){
+    for (i = 0; i < limit; i++) {
 
-        $("#pic"+ j).attr("src", recipes[i].image);
+        $("#pic" + j).attr("src", recipes[i].image);
         $("#title" + j).text(recipes[i].title);
         $("#rating" + j).text("Rating: " + recipes[i].spoonacularScore + "/100");
 
-        if(parseInt(recipes[i].pricePerServing) <= 100){
+        if (parseInt(recipes[i].pricePerServing) <= 100) {
             $("#price" + j).text("$");
-        }else if(parseInt(recipes[i].pricePerServing) > 100 && parseInt(recipes[i].pricePerServing) < 600 ){
+        } else if (parseInt(recipes[i].pricePerServing) > 100 && parseInt(recipes[i].pricePerServing) < 600) {
             $("#price" + j).text("$$");
-        }else{
+        } else {
             $("#price" + j).text("$$$");
         }
 
@@ -112,7 +111,7 @@ function populateResults(recipes){
 }
 
 //Event Listeners
-$(searchBtn).on("click",function(event){
+$(searchBtn).on("click", function(event) {
 
     event.preventDefault();
     // getResults();
@@ -120,63 +119,63 @@ $(searchBtn).on("click",function(event){
 
 });
 
-$(".filterImage").on("click", function(event){
+$(".filterImage").on("click", function(event) {
 
-    event.preventDefault();
-    var id = $(this).attr("id");
-    console.log(id);
+        event.preventDefault();
+        var id = $(this).attr("id");
+        console.log(id);
 
-    var parameters = {};
+        var parameters = {};
 
-    if (id === "spicy"){
-        parameters = {
-            ingredients: "",
-            name: "",
-            cuisine: "african",
-            category: ""
+        if (id === "spicy") {
+            parameters = {
+                ingredients: "",
+                name: "",
+                cuisine: "african",
+                category: ""
+            }
+        } else if (id === "savory") {
+            parameters = {
+                ingredients: "onion",
+                name: "",
+                cuisine: "",
+                category: ""
+            }
+
+        } else if (id === "onepot") {
+            parameters = {
+                ingredients: "",
+                name: "casserole",
+                cuisine: "",
+                category: ""
+            }
+
+        } else if (id === "special") {
+            parameters = {
+                ingredients: "",
+                name: "",
+                cuisine: "french",
+                category: ""
+            }
+        } else if (id === "surprise") {
+            parameters = {
+                ingredients: "",
+                name: "",
+                cuisine: "",
+                category: "main course"
+            }
+        } else if (id === "healthy") {
+            parameters = {
+                ingredients: "",
+                name: "",
+                cuisine: "",
+                category: ""
+            }
         }
-    }else if(id === "savory"){
-        parameters = {
-            ingredients: "onion",
-            name: "",
-            cuisine: "",
-            category: ""
-        }
 
-    }else if(id === "onepot"){
-        parameters = {
-            ingredients: "",
-            name: "casserole",
-            cuisine: "",
-            category: ""
-        }
+    }) //End of filterImage click event
 
-    }else if(id === "special"){
-        parameters = {
-            ingredients: "",
-            name: "",
-            cuisine: "french",
-            category: ""
-        }
-    }else if(id === "surprise"){
-        parameters = {
-            ingredients: "",
-            name: "",
-            cuisine: "",
-            category: "main course"
-        }
-    }else if(id === "healthy"){
-        parameters = {
-            ingredients: "",
-            name: "",
-            cuisine: "",
-            category: ""
-        }
-    }
-
-})//End of filterImage click event
-
-$(".result").on("click", function(event){
+$(".result").on("click", function(event) {
 
     event.preventDefault();
     // console.log("Result clicked");
@@ -193,7 +192,7 @@ $(".result").on("click", function(event){
 
     var ingredients = [];
 
-    for(i = 0; i < recipe.extendedIngredients.length; i++ ){
+    for (i = 0; i < recipe.extendedIngredients.length; i++) {
         ingredients.push(recipe.extendedIngredients[i].name);
     }
     // ingredients = recipe.extendedIngredients;
@@ -213,5 +212,3 @@ $(".result").on("click", function(event){
     var backgroundImage = "none";
 
 });
-
-
